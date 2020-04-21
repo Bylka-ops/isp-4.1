f3ec6fad44c037a6eb2e73244aee5689b934e076
AmigoElMato <artem.papikyan.00@bk.ru>
2020-04-21 15:00:54 +0300

    first commit

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Management;
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Версия Windows: "+ Environment.OSVersion);
+            Console.WriteLine("64 Bit операционная система? : "+ Environment.Is64BitOperatingSystem);
+            Console.WriteLine("Имя компьютера : "+Environment.MachineName);
+            Console.WriteLine("Число процессоров : " + Environment.ProcessorCount);
+            Console.WriteLine("Системная папка : "+Environment.SystemDirectory);
+            Console.ReadKey();
+        }
+    }
+}
